83f233c5646f4c9130749bda07259c5f858d8a40
Kevin Oliva <kevin.k.oliva@gmail.com>
2025-12-03 19:12:15 -0600

    allow for key-values and key-arrays

--- Heroes.Element/JsonTypeInfoResolvers/GameStringModifier/GameStringFilePropertyId.cs
+++ Heroes.Element/JsonTypeInfoResolvers/GameStringModifier/GameStringFilePropertyId.cs
@@ -3,9 +3,15 @@
 /// <summary>
 /// Represents a dictionary of game string file property ids for the gamestring json file.
 /// </summary>
-public class GameStringFilePropertyId //: SortedDictionary<string, GameStringText>;
+public class GameStringFilePropertyId
 {
+    /// <summary>
+    /// Gets or sets the key-value pairs of <see cref="GameStringText"/>s.
+    /// </summary>
     public SortedDictionary<string, GameStringText> KeyValuePairs { get; set; } = [];
 
+    /// <summary>
+    /// Gets or sets the key-array pairs of <see cref="GameStringText"/>s.
+    /// </summary>
     public SortedDictionary<string, List<GameStringText>> KeyArrayPairs { get; set; } = [];
 }
