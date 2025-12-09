8f8d72308665435b25e84a56dbb13bf78c7717ef
Kevin Oliva <kevin.k.oliva@gmail.com>
2025-12-08 21:19:34 -0600

    added test for announcer serializer

---
+++ Tests/Heroes.Element.Tests/_Serialization/AnnouncerSerializerTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Heroes.Element.Serialization.Tests;
+
+[TestClass]
+public class AnnouncerSerializerTests
+{
+    [TestMethod]
+    public void Serialize_AllPropertiesSet_ReturnsJson()
+    {
+        // arrange
+        Announcer announcer = new("id")
+        {
+            Name = new GameStringText("Test Announcer"),
+            Description = new GameStringText("Test Description"),
+            SortName = new GameStringText("SortName"),
+            HyperlinkId = "hyperlink_id",
+            Image = "test.png",
+            Gender = "Male",
+            Rarity = Rarity.Legendary,
+            ReleaseDate = new DateOnly(2024, 1, 1),
+            AttributeId = "ABDD",
+            Category = "category",
+            Event = "an event",
+            Franchise = Franchise.Starcraft,
+            HeroId = "AI",
+            SearchText = new GameStringText("item1 item2"),
+            ShowInStore = true,
+        };
+
+        // act
+        string json = JsonSerializer.Serialize(announcer, SerializerSettings.SetJsonSerializerDataOptions());
+
+        // assert
+        json.Should().Be(
+            """
+            {
+              "name": "Test Announcer",
+              "sortName": "SortName",
+              "hyperlinkId": "hyperlink_id",
+              "attributeId": "ABDD",
+              "franchise": "Starcraft",
+              "rarity": "Legendary",
+              "releaseDate": "2024-01-01",
+              "category": "category",
+              "event": "an event",
+              "showInStore": true,
+              "gender": "Male",
+              "heroId": "AI",
+              "image": "test.png",
+              "searchText": "item1 item2",
+              "description": "Test Description"
+            }
+            """);
+    }
+}
---
+++ Tests/Heroes.Element.Tests/_Serialization/SerializerSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace Heroes.Element.Serialization.Tests;
+
+public static class SerializerSettings
+{
+    public static JsonSerializerOptions SetJsonSerializerDataOptions()
+    {
+        return new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            Converters =
+            {
+                new JsonStringEnumConverter(),
+                new DoubleRoundingConverter(),
+                new LinkIdConverter(),
+                new AbilityLinkIdConverter(),
+                new TalentLinkIdConverter(),
+                new GameStringTextConverter(gameStringTextType: GameStringTextType.RawText),
+                new HeroesDataVersionConverter(),
+            },
+            TypeInfoResolver = new HeroesElementResolver()
+            {
+                Modifiers =
+                {
+                    typeInfo => JsonTypeInfoModifiers.SerializationModifiers(typeInfo, LocalizedTextOption.None, []),
+                },
+            },
+        };
+    }
+}
