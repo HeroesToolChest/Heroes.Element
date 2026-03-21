969f66f1888825d3b29ca76396a603a0b28425fc
Kevin Oliva <kevin.k.oliva@gmail.com>
2026-03-20 23:10:09 -0500

    update

--- Heroes.Element/Models/TextureSheet.cs
+++ Heroes.Element/Models/TextureSheet.cs
@@ -6,7 +6,7 @@
 public class TextureSheet
 {
     /// <summary>
-    /// Gets or sets the original image file name with path (as .dds).
+    /// Gets or sets the original image file name with path (as .dds). File path separator is the original found data source.
     /// </summary>
     public string? Image { get; set; }
 
