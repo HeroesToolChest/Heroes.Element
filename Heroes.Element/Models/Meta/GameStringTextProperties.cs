b74eb4efd1e46ee398f37168501e511e20693c5b
Kevin Oliva <kevin.k.oliva@gmail.com>
2026-05-03 20:25:45 -0500

    added equality for GameStringTextProperties

--- Heroes.Element/Models/Meta/GameStringTextProperties.cs
+++ Heroes.Element/Models/Meta/GameStringTextProperties.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Contains information about the gamestring text.
 /// </summary>
-public class GameStringTextProperties
+public class GameStringTextProperties : IEquatable<GameStringTextProperties>
 {
     /// <summary>
     /// Gets or sets the locale of the data (e.g., "enUS", "frFR").
@@ -25,4 +25,34 @@ public class GameStringTextProperties
     /// Gets or sets the properties for the style variables used in the gamestrings.
     /// </summary>
     public StyleVars StyleVars { get; set; } = new();
+
+    /// <inheritdoc/>
+    public bool Equals(GameStringTextProperties? other)
+    {
+        if (other is null)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        return Locale == other.Locale &&
+               GameStringTextType == other.GameStringTextType &&
+               ConstantVars.Replaced == other.ConstantVars.Replaced &&
+               ConstantVars.Preserved == other.ConstantVars.Preserved &&
+               StyleVars.Replaced == other.StyleVars.Replaced &&
+               StyleVars.Preserved == other.StyleVars.Preserved;
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => Equals(obj as GameStringTextProperties);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() =>
+        HashCode.Combine(
+            Locale,
+            GameStringTextType,
+            ConstantVars.Replaced,
+            ConstantVars.Preserved,
+            StyleVars.Replaced,
+            StyleVars.Preserved);
 }
---
+++ Tests/Heroes.Element.Tests/Models/Meta/GameStringTextPropertiesTests.cs
@@ -0,0 +1,267 @@
+﻿namespace Heroes.Element.Models.Meta.Tests;
+
+[TestClass]
+public class GameStringTextPropertiesTests
+{
+    [TestMethod]
+    public void Equals_SameReference_ReturnsTrue()
+    {
+        // arrange
+        GameStringTextProperties props = new();
+
+        // act
+        bool result = props.Equals(props);
+
+        // assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Equals_NullOther_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props = new();
+
+        // act
+        bool result = props.Equals(null);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_DefaultInstances_ReturnsTrue()
+    {
+        // arrange
+        GameStringTextProperties props1 = new();
+        GameStringTextProperties props2 = new();
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Equals_DifferentLocale_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { Locale = StormLocale.ENUS };
+        GameStringTextProperties props2 = new() { Locale = StormLocale.DEDE };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_DifferentGameStringTextType_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { GameStringTextType = GameStringTextType.RawText };
+        GameStringTextProperties props2 = new() { GameStringTextType = GameStringTextType.ColoredText };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_NullAndNonNullGameStringTextType_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { GameStringTextType = null };
+        GameStringTextProperties props2 = new() { GameStringTextType = GameStringTextType.RawText };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_BothNullGameStringTextType_ReturnsTrue()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { GameStringTextType = null };
+        GameStringTextProperties props2 = new() { GameStringTextType = null };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Equals_DifferentConstantVarsReplaced_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { ConstantVars = new() { Replaced = true } };
+        GameStringTextProperties props2 = new() { ConstantVars = new() { Replaced = false } };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_DifferentConstantVarsPreserved_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { ConstantVars = new() { Preserved = true } };
+        GameStringTextProperties props2 = new() { ConstantVars = new() { Preserved = false } };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_DifferentStyleVarsReplaced_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { StyleVars = new() { Replaced = true } };
+        GameStringTextProperties props2 = new() { StyleVars = new() { Replaced = false } };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_DifferentStyleVarsPreserved_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { StyleVars = new() { Preserved = true } };
+        GameStringTextProperties props2 = new() { StyleVars = new() { Preserved = false } };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_AllPropertiesEqual_ReturnsTrue()
+    {
+        // arrange
+        GameStringTextProperties props1 = new()
+        {
+            Locale = StormLocale.FRFR,
+            GameStringTextType = GameStringTextType.PlainText,
+            ConstantVars = new() { Replaced = true, Preserved = false },
+            StyleVars = new() { Replaced = false, Preserved = true },
+        };
+
+        GameStringTextProperties props2 = new()
+        {
+            Locale = StormLocale.FRFR,
+            GameStringTextType = GameStringTextType.PlainText,
+            ConstantVars = new() { Replaced = true, Preserved = false },
+            StyleVars = new() { Replaced = false, Preserved = true },
+        };
+
+        // act
+        bool result = props1.Equals(props2);
+
+        // assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Equals_ObjectNull_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props = new();
+
+        // act
+        bool result = props.Equals((object?)null);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_ObjectDifferentType_ReturnsFalse()
+    {
+        // arrange
+        GameStringTextProperties props = new();
+
+        // act
+        bool result = props.Equals("not a GameStringTextProperties");
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_ObjectSameValues_ReturnsTrue()
+    {
+        // arrange
+        GameStringTextProperties props1 = new();
+        GameStringTextProperties props2 = new();
+
+        // act
+        bool result = props1.Equals((object)props2);
+
+        // assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void GetHashCode_EqualObjects_ReturnsSameHashCode()
+    {
+        // arrange
+        GameStringTextProperties props1 = new()
+        {
+            Locale = StormLocale.ENUS,
+            GameStringTextType = GameStringTextType.RawText,
+            ConstantVars = new() { Replaced = true, Preserved = false },
+            StyleVars = new() { Replaced = false, Preserved = true },
+        };
+
+        GameStringTextProperties props2 = new()
+        {
+            Locale = StormLocale.ENUS,
+            GameStringTextType = GameStringTextType.RawText,
+            ConstantVars = new() { Replaced = true, Preserved = false },
+            StyleVars = new() { Replaced = false, Preserved = true },
+        };
+
+        // act
+        int hashCode1 = props1.GetHashCode();
+        int hashCode2 = props2.GetHashCode();
+
+        // assert
+        hashCode1.Should().Be(hashCode2);
+    }
+
+    [TestMethod]
+    public void GetHashCode_DifferentObjects_ReturnsDifferentHashCode()
+    {
+        // arrange
+        GameStringTextProperties props1 = new() { Locale = StormLocale.ENUS };
+        GameStringTextProperties props2 = new() { Locale = StormLocale.DEDE };
+
+        // act
+        int hashCode1 = props1.GetHashCode();
+        int hashCode2 = props2.GetHashCode();
+
+        // assert
+        hashCode1.Should().NotBe(hashCode2);
+    }
+}
